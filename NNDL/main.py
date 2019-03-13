bea8965c01c506bfc8397484c6bfb40d490ec524
couyang24 <30837746+couyang24@users.noreply.github.com>
2019-03-13 14:31:34 -0400

    update

--- NNDL/main.py
+++ NNDL/main.py
@@ -68,3 +68,27 @@ a.T.shape
 
 print(np.dot(a, a.T))
 print(np.dot(a.T, a))
+
+
+a = np.random.randn(2, 3) # a.shape = (2, 3)
+b = np.random.randn(2, 1) # b.shape = (2, 1)
+c = a + b
+
+del c
+# a.shape = (3,4)
+# b.shape = (4,1)
+
+c = np.zeros(12).reshape(3,4)
+a = np.random.randn(3, 4)
+b = np.random.randn(4, 1)
+for i in range(3):
+  for j in range(4):
+    c[i][j] = a[i][j] + b[j]
+
+a + b.T
+
+
+
+a = np.random.randn(3, 3)
+b = np.random.randn(3, 1)
+c = a*b
